238214679c2dd9945d322c1c707c2531ebb57422
jw <semanticist0@gmail.com>
2024-08-02 09:21:07 +0900

    fix: path problem

--- apps/host/tailwind.config.js
+++ apps/host/tailwind.config.js
@@ -1,6 +1,8 @@
 const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
 const { join } = require('path');
 
+const sharedConfig = require('../../tailwind.config');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -8,10 +10,12 @@ module.exports = {
       __dirname,
       '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
     ),
+    join(__dirname, '../../node_modules/rizzui/dist/*.{js,ts,jsx,tsx}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  presets: [sharedConfig],
 };
